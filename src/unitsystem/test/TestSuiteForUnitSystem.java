1e37aca6ec058be975ab38251ac8ebfdc01eca05
saipratap99 <122005067@sastra.ac.in>
2022-03-23 23:32:33 +0530

    added test suit for unit system

---
+++ src/unitsystem/test/TestSuiteForUnitSystem.java
@@ -0,0 +1,12 @@
+package unitsystem.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({LengthTest.class, WeightTest.class, TemperatureTest.class})
+
+public class TestSuiteForUnitSystem {
+	
+}
---
+++ src/unitsystem/test/TestSuiteRunnerForUnitSystem.java
@@ -0,0 +1,17 @@
+package unitsystem.test;
+
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestSuiteRunnerForUnitSystem {
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(TestSuiteForUnitSystem.class);
+		if(result.getFailureCount() == 0)
+			System.out.println("All test cases passed");
+		else 
+			for(Failure failure : result.getFailures())
+				System.out.println(failure);
+	}
+}
